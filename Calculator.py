4f30f76fde61139b62824c307d8b633d07521e4f
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-07-06 18:56:57 -0300

    First simple calculator done

--- Calculator.py
+++ Calculator.py
@@ -12,4 +12,39 @@
 
 # Ask user which type of operation wish to use
 
-operation = input("Which operation would like to use ? ")
+operation = str(input("Which operation would like to use ? "))
+
+# Ask user what number would like to choose first and then the second
+
+numb1 = float(input("What is your first number would you like choose ? "))
+numb2 = float(input("What is your second number would you like choose ? "))
+
+# Testing...
+# print(operation)
+# print(numb1)
+# print(numb2)
+# Test done!
+
+if(operation == "+"):
+    total = numb1 + numb2
+    print("The sum is: ", total)
+elif(operation == "-"):
+    total = numb1 - numb2
+    print("The subtraction is: ", total)
+elif(operation == "*"):
+    total = numb1 * numb2
+    print("The multiplication is: ", total)
+elif(operation == "/"):
+    total = numb1 / numb2
+    print("The division is: ", total)
+elif(operation == "**"):
+    total = numb1 ** numb2
+    print("The exponentiation is: ", total)
+elif(operation == "%"):
+    total = numb1 % numb2
+    print("The module is: ", total)
+
+# The first simple calculator was done
+
+# Let's do an advanced calculator
+
